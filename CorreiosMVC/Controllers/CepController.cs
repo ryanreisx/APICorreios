55b66c46d149ae3d163698733fa8c2c505fea256
Ryan <ryanreis280903@gmail.com>
2023-09-24 00:04:24 -0300

    first commit

---
+++ CorreiosMVC/Controllers/CepController.cs
@@ -0,0 +1,47 @@
+﻿using CorreiosMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CorreiosMVC.Controllers
+{
+    public class CepController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public CepController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCepInfo(string cep)
+        {
+            try
+            {
+                var viaCepUrl = $"http://viacep.com.br/ws/{cep}/json/";
+
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetFromJsonAsync<ViaCepModel>(viaCepUrl);
+
+                return View(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
